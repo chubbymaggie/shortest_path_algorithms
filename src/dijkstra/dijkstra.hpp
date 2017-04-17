af9e0c73a0214808c460c5bf17d64d93cfe19757
Mike Weyandt <mike@mikeweyandt.com>
2017-04-16 23:15:05 -0400

    dumb graph and dijkstra shortest path

--- src/dijkstra/dijkstra.hpp
+++ src/dijkstra/dijkstra.hpp
@@ -1,11 +1,58 @@
-#include "../undirected_graph/undirected_graph.hpp"
+#include "../directed_graph/directed_graph.hpp"
+#include <vector>
+#include <iostream>
+#include <queue>
+#include <climits>
 
 struct Dijkstra {
-  undirected_graph<int, int> G;
-  vertex<int> V;
+  directed_graph G;
+  int src;
 
-  Dijkstra(undirected_graph<int, int> & G, vertex<int> & V) : G(G), V(V) { }
 
+  Dijkstra(directed_graph & G, int src) : G(G), src(src) { }
+  
+  std::vector<int> shortest_path();  
   
 
+
 }; 
+
+
+
+std::vector<int> Dijkstra::shortest_path() {
+
+
+  std::priority_queue<std::pair<int,int> , std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>> > queue;
+  std::vector<int> distance(2000000, INT_MAX);
+
+  queue.push(std::make_pair(0, src));
+  distance[src] = 0;
+
+  while(!queue.empty()) {
+    int u = queue.top().second;
+    queue.pop();
+
+    for(auto i = G.list[u].begin(); i != G.list[u].end(); i++) {
+
+      int v = (*i).first;
+      int weight = (*i).second;
+
+     if(distance[v] > distance[u] + weight) {
+       distance[v] = distance[u] + weight;
+         queue.push(std::make_pair(distance[v], v));
+     }
+
+   
+  }
+}
+  for(int i = 0; i < 2000000; i++)
+    std::cout << i << " --> " << distance[i] << "\n";
+  
+
+
+
+  return distance;
+}
+
+
+
--- src/dijkstra/main.cpp
+++ src/dijkstra/main.cpp
@@ -1,17 +1,42 @@
 #include "dijkstra.hpp"
-#include "../undirected_graph/undirected_graph.hpp"
+#include "../directed_graph/directed_graph.hpp"
+#include <algorithm>
+#include <random>
+#include <chrono>
 
 int main() {
-  auto G = undirected_graph<int, int>(); 
 
-  G.add_vertex(0);
-  G.add_vertex(1);
-  G.add_vertex(2);
-  G.add_vertex(3);
-  G.add_edge(0, 1, 10);
-  G.add_edge(0, 3, 9);
-  G.add_edge(0, 2, 7);
-  G.add_edge(2, 3, 1);
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 
+  auto G = directed_graph(2000000); 
+
+  std::vector<int> nodes1;
+  std::vector<int> nodes2;
+
+  std::vector<int> weights;
+
+
+  for(int i = 0; i < 2000000; i++)
+    nodes1.push_back(i);
+
+  for(int i = 1; i < 2000000; i++)
+    nodes2.push_back(i);
+
+  for(int i = 0; i < 2000000; i ++)
+    weights.push_back(rand() % 10);
+
+   //std::shuffle(nodes1.begin(), nodes1.end(), std::default_random_engine(seed));
+   //std::shuffle(nodes2.begin(), nodes2.end(), std::default_random_engine(seed));
+   std::shuffle(weights.begin(), weights.end(), std::default_random_engine(seed));
+
+
+   for(int i = 0; i < 2000000; i++) {
+     G.add_edge(nodes1[i], nodes2[i], weights[i]);
+   }
+
+  auto d = Dijkstra(G, 0);
+
+  d.shortest_path();
+  
   return 0;
 };
